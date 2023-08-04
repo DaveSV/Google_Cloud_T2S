82e050346dbc5470aaf26d8805a85e7942bfe0af
DaveSV <alb.saenz@gmail.com>
2023-08-03 19:59:06 -0600

    Agregado .gitignore y excluidos venv y zinc-direction-238121-45cc42b35b4c.json

---
+++ README.md
@@ -0,0 +1,1 @@
+# Google_Cloud_T2S
---
+++ synthesize_text.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# All Rights Reserved.
+
+"""Google Cloud Text-To-Speech API sample application .
+
+Example usage:
+    python synthesize_text.py --text "hello"
+    python synthesize_text.py --ssml "<speak>Hello there.</speak>"
+"""
+
+import argparse
+import os
+
+# Define la variable de entorno para apuntar al archivo JSON de las credenciales.
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "zinc-direction-238121-45cc42b35b4c.json"
+
+# [START tts_synthesize_text]
+def synthesize_text(text):
+    """Synthesizes speech from the input string of text."""
+    from google.cloud import texttospeech
+
+    client = texttospeech.TextToSpeechClient()
+
+    input_text = texttospeech.SynthesisInput(text=text)
+
+    # Note: the voice can also be specified by name.
+    # Names of voices can be retrieved with client.list_voices().
+    voice = texttospeech.VoiceSelectionParams(
+        language_code="es-US",
+        name="es-US-Neural2-B",
+        ssml_gender=texttospeech.SsmlVoiceGender.MALE,
+    )
+
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
+
+    response = client.synthesize_speech(
+        request={"input": input_text, "voice": voice, "audio_config": audio_config}
+    )
+
+    # The response's audio_content is binary.
+    with open("output.mp3", "wb") as out:
+        out.write(response.audio_content)
+        print('Audio content written to file "output.mp3"')
+
+
+# [END tts_synthesize_text]
+
+
+# [START tts_synthesize_ssml]
+def synthesize_ssml(ssml):
+    """Synthesizes speech from the input string of ssml.
+
+    Note: ssml must be well-formed according to:
+        https://www.w3.org/TR/speech-synthesis/
+
+    Example: <speak>Hello there.</speak>
+    """
+    from google.cloud import texttospeech
+
+    client = texttospeech.TextToSpeechClient()
+
+    input_text = texttospeech.SynthesisInput(ssml=ssml)
+
+    # Note: the voice can also be specified by name.
+    # Names of voices can be retrieved with client.list_voices().
+    voice = texttospeech.VoiceSelectionParams(
+        language_code="en-US",
+        name="en-US-Standard-C",
+        ssml_gender=texttospeech.SsmlVoiceGender.FEMALE,
+    )
+
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
+
+    response = client.synthesize_speech(
+        input=input_text, voice=voice, audio_config=audio_config
+    )
+
+    # The response's audio_content is binary.
+    with open("output.mp3", "wb") as out:
+        out.write(response.audio_content)
+        print('Audio content written to file "output.mp3"')
+
+
+# [END tts_synthesize_ssml]
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    group = parser.add_mutually_exclusive_group(required=True)
+
+    # Remueve el argumento "--text" y agrega "--file" para el nombre del archivo
+    group.add_argument("--file", help="Path to the input text file.")
+    group.add_argument("--ssml", help="The ssml string from which to synthesize speech.")
+
+    args = parser.parse_args()
+
+    if args.file:
+        # Leer el contenido del archivo
+        with open(args.file, "r") as file:
+            text = file.read()
+        synthesize_text(text)
+    else:
+        synthesize_ssml(args.ssml)
